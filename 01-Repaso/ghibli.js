41bd8bef178794acbd1a6b191af06ce61468153d
SaulSaVi <1575saavedra@gmail.com>
2020-05-26 22:15:55 -0500

    avance ejercicios repaso request

--- 01-Repaso/ghibli.js
+++ 01-Repaso/ghibli.js
@@ -6,14 +6,23 @@ const request = require('request');
         console.log("Status Code: "+response.statusCode);
         const data = JSON.parse(body)
         const total = [];
-
-        console.log(data[1])
         
-        // for (let i = 0; i < data[i].length; i++){
-        //    total.push(data.title[i]); 
-        //     }
-        // console.log(total);
+        for (let i = 0; i < data.length; i++){
+            console.log(data[i].title); 
+            }
+        
+        //console.log(data[0].title)
         }
     
     request(`https://ghibliapi.herokuapp.com/films`, pelisGhibli)
     
+
+    request.get('https://ghibliapi.herokuapp.com/films', (err, res, body) => {
+  return new Promise((resolve, reject) => {
+    res.statusCode === 200
+      ? resolve(JSON.parse(body).results)
+      : reject(`Error ${res.statusCode}`);
+  });
+})
+  .then( res => {console.log(res)} )  // Función Flecha Optimizada
+  .catch((err) => { return console.log(err);}); // Función Flecha sin optimizar
